debe9a63c6f8e854ce54db974bb0a00362d81448
James Yu <16505087+j-yu@users.noreply.github.com>
2018-06-04 00:18:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, request, jsonify
+from flask_bower import Bower
 from twitter_client import TwitterInterface
+
+
+app = Flask(__name__)
+Bower(app)
 
 client = TwitterInterface()
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/tweets')
 def tweets():
     incoming = request.args.get('query')
-    chosen_screen_name = incoming
-    client.screen_name = chosen_screen_name
-    server_to_client_data = client.grab_texts()
-    return jsonify({'server_to_client_data': server_to_client_data})
+    client.screen_name = incoming
+    data = client.grab_texts()
+
+    return jsonify({'tweets': [i for i in data]})
