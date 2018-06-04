eec6c9a89170ef42d15d8f46d5f5c628a9d75e7a
James Yu <16505087+j-yu@users.noreply.github.com>
2018-06-04 00:15:20 -0400

    Update chart.js

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,44 +1,79 @@
-$('#launch').click(function(){
-    var text = $.get('tweets', {'query': $('#query').val()}, function(data){
-    text_string=' ';
-    var texts = data['server_to_client_data']
-    for(i=0; i<texts.length; i++){
-        text_string+=texts[i].text;
-    }
-    buildChart(text_string);
-});
-
-});
-
-function buildChart(text_string){
-var lines = text_string.split(/[,. ]+/g),
-  data = Highcharts.reduce(lines, function (arr, word) {
-    var obj = Highcharts.find(arr, function (obj) {
-      return obj.name === word;
+$(function() {
+    console.log('document ready');
+    tweets('realdonaldtrump');
+    $('#launch').click(function() {
+        tweets($('#query').val());
     });
-    if (obj) {
-      obj.weight += 1;
-    } else {
-      obj = {
-        name: word,
-        weight: 1
-      };
-      arr.push(obj);
-    }
-    return arr;
-  }, []);
-
-Highcharts.chart('container', {
-
-  series: [{
-    type: 'wordcloud',
-    data: data,
-    name: 'Occurrences',
-    spiral: 'archimedean'
-  }],
-  title: {
-    text: 'from ' + '@' + $('#query').val() + '\'s' + ' last 50 tweets and retweets:'
-  },
-
 });
+
+function tweets(query) {
+    $('.panel-group').empty();
+    var query = query;
+    $.get('tweets', {
+        'query': query
+    }, function(data) {
+        console.log(data);
+        data = data['tweets'];
+        console.log(data);
+        clean = '';
+        for (i = 0; i < data.length; i++) {
+            clean += data[i]['clean'];
+            $('.pre-scrollable').append('<div class="panel-group">' + n() + '</div>');
+        }
+
+        function n() {
+            if (data[i]['sentiment'] == 'positive') {
+                return '<div class="panel panel-success">' + '<div class="panel-heading">' + '@ ' + data[i]['username'] + '</div>' + '<div class="panel-body">' + data[i]['text'] + '</div>' + '</div>';
+            } else if (data[i]['sentiment'] == 'negative') {
+                return '<div class="panel panel-danger">' + '<div class="panel-heading">' + '@ ' + data[i]['username'] + '</div>' + '<div class="panel-body">' + data[i]['text'] + '</div>' + '</div>';
+            } else {
+                return '<div class="panel panel-default">' + '<div class="panel-heading">' + '@ ' + data[i]['username'] + '</div>' + '<div class="panel-body">' + data[i]['text'] + '</div>' + '</div>';
+            }
+        };
+        console.log(clean);
+        buildChart(clean);
+    });
+};
+
+function buildChart(clean) {
+    var lines = clean.split(/[,. ]+/g),
+        data = Highcharts.reduce(lines, function(arr, word) {
+            var obj = Highcharts.find(arr, function(obj) {
+                return obj.name === word;
+            });
+            if (obj) {
+                obj.weight += 1;
+            } else {
+                obj = {
+                    name: word,
+                    weight: 1
+                };
+                arr.push(obj);
+            }
+            return arr;
+        }, []);
+    Highcharts.chart('container', {
+        chart: {
+            type: 'wordcloud',
+            //      backgroundColor: '#F4F6F6',
+        },
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 1600,
+                    maxHeight: 1600
+                }
+            }]
+        },
+        series: [{
+            type: 'wordcloud',
+            borderSize: '10px',
+            data: data,
+            name: 'Frequency'
+            //    spiral: 'archimedean'
+        }],
+        title: {
+            text: 'from ' + '@' + $('#query').val() + '\'s' + ' last 50 tweets and retweets:'
+        },
+    });
 };
